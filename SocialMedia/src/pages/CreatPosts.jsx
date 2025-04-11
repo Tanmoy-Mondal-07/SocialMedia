a407c66c4dbb2c2095e9ba9269dee4ec030e2f84
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-04-11 23:44:43 +0530

    add post

--- SocialMedia/src/pages/CreatPosts.jsx
+++ SocialMedia/src/pages/CreatPosts.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
 import postConfigServices from '../appwrite/postConfig'
 import { PostHandler } from '../component';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { hideLoading, showLoading } from '../store/LodingState';
 
 function CreatPosts() {
+  const navigate = useNavigate()
+  const userData = useSelector((state) => state.auth.userData)
+  const dispatch = useDispatch()
 
   const handlePostSubmit = async (data) => {
-    console.log(data);
-    // try {
-    //   const postinfo = await postConfigServices.createPost({
-    //     userId:'100',
-    //     content:'is it working',
-    //   })
-    //   console.log(postinfo);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    dispatch(showLoading());
+    try {
+      let mediaUrl = null
+
+      if (data.media) {
+        const uploadInfo = await postConfigServices.uploadFile(data.media)
+        if (uploadInfo) {
+          mediaUrl = uploadInfo.$id
+        }
+      }
+
+      const postinfo = await postConfigServices.createPost({
+        userId: userData.$id,
+        content: data.content,
+        mediaUrl: mediaUrl,
+        visibility: data.visibility,
+        title: data.title,
+      })
+      if (postinfo) {
+        navigate(`/profile/${userData.$id}`)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      dispatch(hideLoading());
+    }
   }
 
 
   return (
     <>
       <div className="min-h-screen p-6">
-      <PostHandler onSubmit={handlePostSubmit}/>
+        <PostHandler onSubmit={handlePostSubmit} />
       </div>
     </>
   )
@@ -36,3 +58,8 @@ export default CreatPosts
           initialVisibility="private"
           onSubmit={handlePostSubmit}
         /> */}
+
+//content:"anranranrtn"
+//media:File { name: 'ab67616d0000b273451f108719b7161287731033.jpg', lastModified: 1741010408999, lastModifiedDate: Mon Mar 03 2025 19: 30:08 GMT +0530(India Standard Time), webkitRelativePath: '', size: 93599, … }
+//title:"zbaeb"
+//visibility:"public"
