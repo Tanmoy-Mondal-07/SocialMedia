34bf82b4835ccac28cd787895881fc23d78220d4
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-05-07 22:41:44 +0530

    design change

--- SocialMedia/src/pages/ChatPage.jsx
+++ SocialMedia/src/pages/ChatPage.jsx
@@ -6,6 +6,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { Query } from "appwrite";
 import getProfilesByCache from "../utils/getProfilesThroughache";
 import { ArrowLeft } from "lucide-react";
+import { Button } from "../component";
 
 export default function ChatPage() {
   const navigate = useNavigate()
@@ -58,9 +59,9 @@ export default function ChatPage() {
   }, [messages]);
 
   return senderid && (
-    <div className="w-full mb-0 rounded-lg max-w-xl mx-auto h-svh sm:h-screen flex flex-col sm:p-4 bg-bground-100 text-fground-200 shadow-md animate-fadeIn">
+    <div className="w-full rounded-lg mx-auto h-svh flex flex-col bg-bground-200 text-fground-200 shadow-md animate-fadeIn">
       {/* Header */}
-      <div className="flex items-center gap-3 p-3 bg-white shadow rounded-lg animate-slideIn">
+      <div className="flex items-center gap-3 p-3 bg-white shadow animate-slideIn">
         <button onClick={() => navigate(-1)}><ArrowLeft /></button>
         <img
           src={senderProfile?.profilePic}
@@ -79,7 +80,7 @@ export default function ChatPage() {
           >
             <div
               className={`inline-block max-w-[75%] px-4 py-2 rounded-full shadow text-sm sm:text-base animate-fadeInUp ${msg.senderid === senderid
-                ? "bg-mground-100 text-white rounded-tr-none"
+                ? "bg-fground-100 text-white rounded-tr-none"
                 : "bg-white text-gray-900 border border-gray-300 rounded-tl-none"
                 }`}
             >
@@ -93,20 +94,20 @@ export default function ChatPage() {
       {/* Input */}
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex gap-2 p-2 sm:p-4 bg-white rounded-lg shadow mt-2 animate-slideIn"
+        className="flex gap-2 p-2 sm:p-4 bg-white mb-1.5 sm:mb-0 shadow mt-2 animate-slideIn"
       >
         <input
           type="text"
           {...register("message", { required: true })}
           placeholder="Type a message..."
-          className="flex-1 px-3 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 text-sm sm:text-base"
+          className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-sm sm:text-base"
         />
-        <button
+        <Button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors text-sm sm:text-base"
+          // className="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors text-sm sm:text-base"
         >
           Send
-        </button>
+        </Button>
       </form>
     </div>
   );
