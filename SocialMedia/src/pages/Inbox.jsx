59c4353726ae9d98ae7225952e70994b42a3fcb7
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-05-10 22:28:53 +0530

    ..

--- SocialMedia/src/pages/Inbox.jsx
+++ SocialMedia/src/pages/Inbox.jsx
@@ -7,11 +7,11 @@ export default function Inbox() {
   const list = useSelector((state) => state.inbox.resivedUserList)
 
   return currentUserId && (
-    <div className="max-w-md mx-auto shadow-lg rounded-lg overflow-hidden bg-body-0">
+    <div className="max-w-xl mx-auto shadow-lg rounded-lg overflow-hidden bg-body-0">
       <div className="border-b px-4 py-3">
         <h2 className="text-xl font-semibold">Inbox</h2>
       </div>
-      <div className="h-80 overflow-y-auto">
+      <div className="min-h-80 overflow-y-auto">
         <ul className="list-none m-0 p-0">
           {list.map((senderIds) => (
             (senderIds === currentUserId) ? null :
